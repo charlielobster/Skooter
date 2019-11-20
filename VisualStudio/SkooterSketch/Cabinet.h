081114e2d5309d6297554feb4974f3aed6bdd407
Erik Brobyn <erik.brobyn@icloud.com>
2019-11-20 18:14:36 -0500

    comments

--- VisualStudio/SkooterSketch/Cabinet.h
+++ VisualStudio/SkooterSketch/Cabinet.h
@@ -7,31 +7,13 @@
 class Cabinet
 {
 public:
-	/*
-	  SD card test
+	//SD card attached to SPI bus as follows:
+	//** MOSI - pin 11 on Arduino Uno/Duemilanove/Diecimila
+	//** MISO - pin 12 on Arduino Uno/Duemilanove/Diecimila
+	//** CLK - pin 13 on Arduino Uno/Duemilanove/Diecimila
+	//** CS - pin 10
 
-	  This example shows how use the utility libraries on which the'
-	  SD library is based in order to get info about your SD card.
-	  Very useful for testing a card when you're not sure whether its working or not.
-
-	  The circuit:
-		SD card attached to SPI bus as follows:
-	 ** MOSI - pin 11 on Arduino Uno/Duemilanove/Diecimila
-	 ** MISO - pin 12 on Arduino Uno/Duemilanove/Diecimila
-	 ** CLK - pin 13 on Arduino Uno/Duemilanove/Diecimila
-	 ** CS - depends on your SD card shield or module.
-			Pin 4 used here for consistency with other Arduino examples
-
-
-	  created  28 Mar 2011
-	  by Limor Fried
-	  modified 9 Apr 2012
-	  by Tom Igoe
-	*/
-	// include the SD library:
-
-
-// set up variables using the SD utility library functions:
+	// set up variables using the SD utility library functions:
 	Sd2Card card;
 	SdVolume volume;
 	SdFile root;
