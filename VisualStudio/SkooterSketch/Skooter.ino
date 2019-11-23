ce65c9020abeff7ec03b9593d3d96a379dd21499
AlexandrNai <57844302+AlexandrNai@users.noreply.github.com>
2019-11-23 15:22:54 -0500

    Calibration of Tilt and Pan

--- VisualStudio/SkooterSketch/Skooter.ino
+++ VisualStudio/SkooterSketch/Skooter.ino
@@ -39,3 +39,85 @@ void Skooter::doStuff() // function "do stuff" belongs to the class "Skooter"
 
 	//delay(500);
 }
+
+//FIXME ONLY RUN IF IN FRONT OF WALL
+int calTilt() {
+  int calibratedTilt = minTilt;
+  float minDistance = 9999.9;
+
+  pan.write(calibratedPan);
+  delay(250);
+  tilt.write(calibratedTilt);
+  delay(250);
+
+  for (int tiltAngle = minTilt; tiltAngle <= maxTilt; tiltAngle++) {
+    tilt.write(tiltAngle);
+    delay(15);
+
+    int sum = eye.distance();
+    for (int i = 0; i < 9; i++) {
+      sum = sum + eye.distance(false);
+    }
+    float avgDistance = sum / 10.0;
+
+    if (avgDistance < minDistance) {
+      minDistance = avgDistance;
+      calibratedTilt = tiltAngle;
+    }
+
+  }
+
+  tilt.write(calibratedTilt);
+  delay(250);
+  
+  return calibratedTilt;
+}
+
+
+int calingPan() {
+  int calibratedPan = 0;
+  float minDistance = 9999.9;
+
+  pan.write(calibratedPan);
+  delay(250);
+  tilt.write(calibratedTilt);
+  delay(250);
+
+  for (int panAngle = 0; panAngle < 180; panAngle++) {
+    pan.write(panAngle);
+    delay(15);
+
+    int sum = eye.distance();
+    for (int i = 0; i < 9; i++) {
+      sum = sum + eye.distance(false);
+    }
+    float avgDistance = sum / 10.0;
+
+    if (avgDistance < minDistance) {
+      minDistance = avgDistance;
+      calibratedPan = panAngle;
+    }
+
+  }
+
+  pan.write(calibratedPan);
+  delay(250);
+  
+  return calibratedPan;
+
+}
+
+int calPan() {
+  calingPan();
+  while (calibratedPan < 85 || calibratedPan > 95) {
+    if (calibratedPan < 85) {
+      tracks.turnLeft(90-/*calibratedPan*/); //FIXME turnRight has to be able to take in a degree and turn long enough to reach that degree then stop
+    }
+    else if (calibratedPan > 95) {
+      tracks.turnRight(180-/*calibratedPan*/); //FIXME as above and also check which direction each one is
+    }
+    calingPan();
+  }
+  
+  return calibratedPan;
+}
