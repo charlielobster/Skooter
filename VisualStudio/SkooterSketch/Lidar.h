033d33c306c4bf1b12c2f98e77370de4a602032e
Erik Brobyn <erik.brobyn@icloud.com>
2019-11-23 22:55:21 -0500

    comments

--- VisualStudio/SkooterSketch/Lidar.h
+++ VisualStudio/SkooterSketch/Lidar.h
@@ -3,16 +3,18 @@
 // https://en.wikipedia.org/wiki/Include_guard
 #define _LIDAR_
 
-#include <LIDARLite.h> //includes Lidarlite library making it available to the header file "Lidar.h"
+#include <LIDARLite.h> 
+// include Lidarlite header files, (making its declarations, types, enums, and static variables) 
+// available to those declared in this header file, "Lidar.h"
 
-class Lidar // creates the class "Lidar"
+class Lidar // declare the class "Lidar"
 {
-public: // functions and variables are accessible outside of the class "Lidar"
+public: // accessible outside of the class "Lidar"
 	void setup(); // declaration of function "setup"
-	int measure(); // declaration of variable "measure" of data type "int"
+	int measure(); // declaration of function "measure" with return type int
 	
-private: // class and instance are NOT accessible outside of the class "Lidar"
-	LIDARLite eye; // creation of the class "Lidarlite" and the instance "eye"
+private: // inaccessible outside of the class "Lidar"
+	LIDARLite eye; // declare Lidarlite instance "eye"
 };
 
 #endif
--- VisualStudio/SkooterSketch/Tracks.ino
+++ VisualStudio/SkooterSketch/Tracks.ino
@@ -1,32 +1,39 @@
 #include "Tracks.h" // gives this implementation file access to the definitions in the header file "Tracks.h"
 
-void Tracks::attach(int f, int t) // the function "attach" belongs to the class (::)  "Tracks"
+// the function "attach" belongs to the class "Tracks"
+// the use of the :: operator indicates a class-level declaration (ie. it is "shared" by all the instances of the class)
+void Tracks::attach(int f, int t)
 {
 	forward.attach(f); // calls the function "attach" (attaches Servo variable to a pin) to the variable, of type Servo, forward
 	turn.attach(t); // calls the function "attach" to the variable (instance), of type Servo, turn
 }
 
-void Tracks::turnRight() // the function "turnRight" belongs to the class (::) "Tracks"
+// the function "turnRight" belongs to the class "Tracks"
+void Tracks::turnRight()
 {
 	turn.write(90 + TURN_SPEED); // calls the function "write" to the variable (instance), of type Servo, "turn"
 }
 
-void Tracks::turnLeft() // the function "turnLeft" belongs to the class (::) "Tracks"
+// the function "turnLeft" belongs to the class "Tracks"
+void Tracks::turnLeft()
 {
 	turn.write(90 - TURN_SPEED); // calls the function "write" (writes a value (speed) to the continuoius rotation servo) to the variable (instance), of type Servo, "turn"; argument of write is a speed
 }
 
-void Tracks::goForward() // the function "goForward" belongs to the class "Tracks"
+// the function "goForward" belongs to the class "Tracks"
+void Tracks::goForward()
 {
 	forward.write(90 + MOVE_SPEED); // calls the function "write" to the variable (instance), of type Servo, "forward"
 }
 
-void Tracks::goBackward() // the function "goBackward" belongs to the class (::) "Tracks"
+// the function "goBackward" belongs to the class "Tracks"
+void Tracks::goBackward()
 {
 	forward.write(90 - MOVE_SPEED); // calls the function "write" to the variable (instance), of type Servo, "forward"
 }
 
-void Tracks::stop() // the function "stop" belongs to the class (::) "Tracks"
+// the function "stop" belongs to the class "Tracks"
+void Tracks::stop()
 {
 	forward.write(90); // calls the function "write" to the variable (instance), of type Servo, "forward"
 	turn.write(90); // calls the function "write" to the variable (instance, of type Servo, "turn"
