f642d6753d001dd0080bd0811e927efb1907fdc2
Erik Brobyn <erik.brobyn@icloud.com>
2019-11-29 15:12:31 -0500

    bugfix

--- VisualStudio/SkooterSketch/SkooterSketch.ino
+++ VisualStudio/SkooterSketch/SkooterSketch.ino
@@ -1,7 +1,14 @@
-void setup() 
+#include "Skooter.h"
+
+Skooter skooter;
+
+void setup()
 {
+	Serial.begin(9600);
+	skooter.setup(5, 3, 9, 6, 28, 118);
 }
 
-void loop() 
+void loop()
 {
+	skooter.doStuff();
 }
